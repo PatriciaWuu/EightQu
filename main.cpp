890f5799f3187dc57a1e1195c9e3523d4a7974c0
PatriciaWuu <lbdqjj@gmail.com>
2020-04-29 18:18:38 -0700

    first

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  EightQ
+//
+//  Created by Patricia Wu on 4/29/20.
+//  Copyright © 2020 Patricia Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+#EightQ
